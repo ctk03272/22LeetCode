dded31c54b379144fb89d49c0947a68e49b890bc
Chun TaeKyung <ctk0327@Naver.com>
2022-09-17 19:00:09 +0900

    Time: 410 ms (19.01%), Space: 34 MB (8.23%) - LeetHub

---
+++ 2022-9-17/278-first-bad-version/278-first-bad-version.kt
@@ -0,0 +1,16 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} */
+
+class Solution: VersionControl() {
+    override fun firstBadVersion(n: Int): Int {
+        var start = 1
+        var end = n
+        while (start < end) {
+            var mid =start + (end-start) / 2
+            if(!isBadVersion(mid)) {
+                start=mid+1
+            }else end=mid
+        }
+        return end
+    }
+}
