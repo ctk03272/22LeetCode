4691e16024d9421373f088bab33ca19a49d1bd80
Chun TaeKyung <ctk0327@Naver.com>
2022-09-20 23:00:50 +0900

    Time: 220 ms (42.59%), Space: 33.3 MB (64.93%) - LeetHub

--- 2022-9-20/509-fibonacci-number/509-fibonacci-number.kt
+++ 2022-9-20/509-fibonacci-number/509-fibonacci-number.kt
@@ -1,17 +1,17 @@
 class Solution {
-    var memo = IntArray(31)
     fun fib(n: Int): Int {
-        if (n == 0) {
-            return 0
-        } else if (n == 1 || n == 2) {
-            return 1;
-        } else {
-            if (memo[n] != 0) {
-                return memo[n]
-            } else {
-                memo[n] = fib(n - 1) + fib(n - 2)
-                return memo[n]
-            }
+        if (n <= 1) return n;
+
+        var cur = 1
+        var sum = 0
+        var temp = 0;
+        var count = 0
+        while (n > count) {
+            temp = cur;
+            cur += sum;
+            sum = temp;
+            count++;
         }
+        return sum
     }
 }
