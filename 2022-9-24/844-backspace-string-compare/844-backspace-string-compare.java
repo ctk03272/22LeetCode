086a6048338ad9f5ccdc681fc6193faa5bc8c1f8
Chun TaeKyung <ctk0327@Naver.com>
2022-09-24 10:16:44 +0900

    Time: 3 ms (57.21%), Space: 40.8 MB (88.64%) - LeetHub

---
+++ 2022-9-24/844-backspace-string-compare/844-backspace-string-compare.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return makeCharacter(s).equals(makeCharacter(t));
+    }
+
+    public String makeCharacter(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char now = s.charAt(i);
+            if (stack.isEmpty()) {
+                if (now != '#') stack.push(now);
+                continue;
+            }
+            if (now == '#') {
+                stack.pop();
+            } else {
+                stack.push(now);
+            }
+        }
+        String answer = "";
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+        return answer;
+    }
+}
