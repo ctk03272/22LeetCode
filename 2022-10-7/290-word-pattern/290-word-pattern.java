c8f8d6854a3dd79b8e9f9a416ba769ce26251046
Chun TaeKyung <ctk0327@Naver.com>
2022-10-07 01:56:40 +0900

    Time: 2 ms (42.08%), Space: 41.3 MB (81.65%) - LeetHub

---
+++ 2022-10-7/290-word-pattern/290-word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        String splitS[] = s.split(" ");
+        if (splitS.length!=pattern.length()){
+            return false;
+        }
+        for (int i = 0; i < splitS.length; i++) {
+            String now=splitS[i];
+            Character nowPatter=pattern.charAt(i);
+            if(!map.containsKey(nowPatter) && !map.containsValue(now)){
+                map.put(nowPatter,now);
+            }else {
+                if(map.containsValue(map)){
+                    return false;
+                }
+                if (!now.equals(map.get(nowPatter))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
