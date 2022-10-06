48539e9fe3dd857e3d9b78d981508720b21ae04e
Chun TaeKyung <ctk0327@Naver.com>
2022-10-07 01:33:22 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (78.15%) - LeetHub

--- 2022-10-7/796-rotate-string/796-rotate-string.java
+++ 2022-10-7/796-rotate-string/796-rotate-string.java
@@ -1,15 +1,6 @@
 class Solution {
-    public boolean rotateString(String s, String goal) {
-        int length = s.length();
-        int count = 0;
-        while (count < length) {
-            char temp = s.charAt(0);
-            s = s.substring(1, s.length()) + temp;
-            if (s.equals(goal)) {
-                return true;
-            }
-            count++;
-        }
-        return false;
+  public boolean rotateString(String s, String goal) {
+        String doubleS = s.concat(s);
+        return s.length()==goal.length() && doubleS.contains(goal);
     }
 }
