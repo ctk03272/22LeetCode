30161abc15358483b2bd7201c6431ec962c6adb5
Chun TaeKyung <ctk0327@Naver.com>
2022-11-20 02:11:44 +0900

    Time: 2 ms (85.79%), Space: 58 MB (31.91%) - LeetHub

---
+++ 2022-11-20/41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int index = 0;
+        while (index < nums.length) {
+            int now=nums[index];
+            if (now<=0 || now>nums.length || now==index+1 || now==nums[now-1]){
+                index++;
+            }else {
+                swapIndex(nums,index,now-1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return nums.length+1;
+    }
+
+    public void swapIndex(int[] nums, int from, int to) {
+        int temp = nums[to];
+        nums[to] = nums[from];
+        nums[from] = temp;
+    }
+}
