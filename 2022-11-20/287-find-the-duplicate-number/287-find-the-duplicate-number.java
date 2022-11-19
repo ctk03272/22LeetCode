a799139863e6f70885f5e89eb7de4ad8945e2e99
Chun TaeKyung <ctk0327@Naver.com>
2022-11-20 02:31:29 +0900

    Time: 140 ms (5.05%), Space: 75.6 MB (59.59%) - LeetHub

---
+++ 2022-11-20/287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int start = 1;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int count = (int) Arrays.stream(nums).filter(value -> value <= mid).count();
+            if (count>mid){
+                end=mid;
+            }else {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
