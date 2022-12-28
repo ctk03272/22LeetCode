871811b851b9e6fad4b1539ccfcc46c83dfdf2e0
Chun TaeKyung <ctk0327@Naver.com>
2022-12-28 23:56:21 +0900

    Time: 4 ms (81.63%), Space: 52.5 MB (87.41%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxarea = 0;
+        int max = 0;
         int left = 0; 
         int right = height.length - 1;
         while (left < right) {
             int width = right - left;
-            maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
+            max = Math.max(max, Math.min(height[left], height[right]) * width);
             if (height[left] <= height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
-        return maxarea;
+        return max;
     }
 }
