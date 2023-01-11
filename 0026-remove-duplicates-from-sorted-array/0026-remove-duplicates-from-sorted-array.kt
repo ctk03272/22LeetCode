8d279d5a40ce24f45aa4ed8ee72ce338bc84ecf8
Chun TaeKyung <ctk0327@Naver.com>
2023-01-12 00:07:47 +0900

    Time: 246 ms (91.93%), Space: 37.8 MB (95.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
@@ -1,8 +1,11 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        var i = 0
+        var i = 1
         nums.forEach {
-            if (i == 0 || it != nums[i - 1]) {
+            if (i==0){
+                nums[i++]=it
+            }
+            if (it != nums[i - 1]) {
                 nums[i++] = it
             }
         }
