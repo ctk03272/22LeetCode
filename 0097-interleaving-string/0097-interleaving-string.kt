d2a51d56da11b85588559745b9c94c9dc7aa656f
Chun TaeKyung <ctk0327@Naver.com>
2023-02-16 01:55:57 +0900

    Time: 176 ms (50.00%), Space: 33.6 MB (96.88%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun isInterleave(s1: String, s2: String, s3: String): Boolean {
+        if (s3.length != s1.length + s2.length) {
+            return false
+        }
+        val dp = Array(s1.length + 1) {
+            BooleanArray(
+                s2.length + 1
+            )
+        }
+        for (i in 0..s1.length) {
+            for (j in 0..s2.length) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]
+                } else {
+                    dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])
+                }
+            }
+        }
+        return dp[s1.length][s2.length]
+    }
+}
